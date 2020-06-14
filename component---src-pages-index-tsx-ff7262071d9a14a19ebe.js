d232d6f328dc6dd035550297927204216d2c98ab
ABK <akollegger@gmail.com>
2020-06-14 22:43:58 +0200

    Updates

---
+++ component---src-pages-index-tsx-ff7262071d9a14a19ebe.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{QeBL:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),r=a("Wbzz"),c=a("Bl7J"),h=a("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(h.a,{title:"Home"}),l.a.createElement("h1",null,"The Path is the Way"),l.a.createElement(r.Link,{to:"/math/"},"Path Math"))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-ff7262071d9a14a19ebe.js.map
