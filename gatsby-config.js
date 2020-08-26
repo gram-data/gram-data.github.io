2cfac7cdec0b6ae83977fb8a862dc834aac5bb82
Andreas Kollegger <akollegger@gmail.com>
2020-08-26 09:16:35 +0200

    converted references to outboundLinks to see where people are leaving to

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-1192232-50",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
+        head: false,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
--- src/pages/references.tsx
+++ src/pages/references.tsx
@@ -2,7 +2,8 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Heading, Styled, Link } from "theme-ui"
+import { Styled, Link } from "theme-ui"
+import { OutboundLink } from "gatsby-plugin-google-analytics"
 
 const IndexPage = () => (
   <Layout>
@@ -10,10 +11,10 @@ const IndexPage = () => (
     <Styled.h1>References</Styled.h1>
 
     <Styled.ul>
-      <Styled.li><Link href="https://neo4j.com/developer/graph-database/">Property Graph Model</Link></Styled.li>
-      <Styled.li><Link href="https://neo4j.com/developer/cypher/">Cypher Query Language</Link></Styled.li>
-      <Styled.li><Link href="https://www.purescript.org">Purescript</Link></Styled.li>
-      <Styled.li><Link href="https://en.wikipedia.org/wiki/Entity_component_system">Entity-component-system</Link></Styled.li>
+      <Styled.li><OutboundLink href="https://neo4j.com/developer/graph-database/">Property Graph Model</OutboundLink></Styled.li>
+      <Styled.li><OutboundLink href="https://neo4j.com/developer/cypher/">Cypher Query Language</OutboundLink></Styled.li>
+      <Styled.li><OutboundLink href="https://www.purescript.org">Purescript</OutboundLink></Styled.li>
+      <Styled.li><OutboundLink href="https://en.wikipedia.org/wiki/Entity_component_system">Entity-component-system</OutboundLink></Styled.li>
     </Styled.ul>
 
   </Layout>
